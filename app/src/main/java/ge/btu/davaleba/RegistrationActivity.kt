38d43d7fe24a46d6367e72f3186a2fc4e683756e
nikolozzoziashvili <61614632+nikolozzoziashvili@users.noreply.github.com>
2021-06-12 21:29:07 +0400

    Davaleba 6

--- app/src/main/java/ge/btu/davaleba/RegistrationActivity.kt
+++ app/src/main/java/ge/btu/davaleba/RegistrationActivity.kt
@@ -7,7 +7,6 @@ import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
-import java.util.regex.Pattern
 
 
 class RegistrationActivity : AppCompatActivity() {
